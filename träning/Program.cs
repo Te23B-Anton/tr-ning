8b4a2507d3cf5625a6ff773a5d6f29e157357769
Anton Faren <Anton.faren@elev.ga.ntig.se>
2025-09-18 12:56:17 +0200

    first commit

---
+++ träning/Program.cs
@@ -0,0 +1,21 @@
+﻿Title();
+
+// Intro();
+
+
+Console.ReadLine();
+
+static void Title()
+{
+    Console.WriteLine("");
+    Console.WriteLine("       _.-;;-._");
+    Console.WriteLine("'-..-'|   ||   |");
+    Console.WriteLine("'-..-'|   ||   |");
+    Console.WriteLine("'-..-'|   ||   |");
+    Console.WriteLine("'-..-'|_.-''-._|");
+}
+
+// static void Intro(){
+//     Console.WriteLine("Det var en gång för länge sedan ...");
+
+// }
